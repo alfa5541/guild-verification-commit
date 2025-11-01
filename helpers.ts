919b9ce82e5c30190220908f593e4fde0f7e023f
cem cengiz <83025810+alfa5541@users.noreply.github.com>
2025-11-01 19:13:09 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T18:36:17.408Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-01T16:13:07.002Z
+Commit: Fix CSS styling bug on mobile devices
